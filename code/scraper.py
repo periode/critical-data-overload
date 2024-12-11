9564073cf15ff905653d812ca132d7770b0c3ad6
période <pierre.depaz@gmail.com>
2024-12-11 22:06:35 +0100

    added code for scraping in python

--- code/scraper.py
+++ code/scraper.py
@@ -1,3 +1,4 @@
+# these two packages need to be installed via `pip install beautifulsoup4 requests`
 from bs4 import BeautifulSoup # this is our parser
 import requests # this is our requester
 
@@ -8,21 +9,23 @@ def parseBody(body):
     soup = BeautifulSoup(body, 'html.parser')    
     results = []
         
-    titleElements = soup.find_all('.cl-static-search-result')
-    # we go through all the 
-    for index, element in titleElements:
+    # we find all <li> elements with class "cl-static-search-result"
+    titleElements = soup.find_all("li", "cl-static-search-result")
+    
+    # we go through all the elements
+    for (index, element) in enumerate(titleElements):
         title = element["title"] # gets the "title" attribute
         link = element.a["href"] # gets the "href" attribute of the <a> tag
         
-        print("#{index}, {title} -> {link}")
+        print(f"#{index}, {title} -> {link}")
         
-        results.append("{index},{title},{link}")
+        results.append(f"{index},{title},{link}")
     
     # and we write our results to a file
     TARGET_FILE = "./results_py.txt"
     with open(TARGET_FILE, 'a') as the_file:
-        the_file.write(",".join(results))
-    print("wrote to {TARGET_FILE}")
+        the_file.write("\n".join(results))
+    print(f"wrote to {TARGET_FILE}")
 
 r = requests.get(TARGET_URL)
 
